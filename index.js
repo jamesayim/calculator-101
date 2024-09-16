e44d41b5a0ede6a389ff23aed9bb9853ce0c7135
jamesyim <theladjames@gmail.com>
2024-09-16 21:24:23 +0000

    initial setup for js file, include functions for operators

---
+++ index.js
@@ -0,0 +1,33 @@
+let firstNumber;
+let operator;
+let lastNumber;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, op, num2) {
+    if (op === "+") {
+        return add(num1, num2);
+    } else if (op === "-") {
+        return subtract(num1, num2);
+    } else if (op === "*") {
+        return multiply(num1, num2);
+    } else if (op === "/") {
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate(2, "+", 2))
