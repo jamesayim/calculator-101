730e950126c79d2c4caa8e173814dc0619de078b
jamesyim <theladjames@gmail.com>
2024-09-17 16:57:39 +0000

    added functionability to buttons to display the numbers

--- index.js
+++ index.js
@@ -30,4 +30,76 @@ function operate(num1, op, num2) {
     }
 }
 
-console.log(operate(2, "+", 2))
+const display = document.querySelector(".display");
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+const del = document.querySelector(".delete");
+
+const dot = document.querySelector(".dot").addEventListener("click", () => {
+    displayValue += ".";
+    display.textContent = displayValue;
+});
+
+const clear = document.querySelector(".clear").addEventListener("click", () => {
+    displayValue = "";
+    display.textContent = displayValue;
+});
+
+// declaration of display value
+let displayValue = "";
+
+one.addEventListener("click", () => {
+    displayValue += 1;
+    display.textContent = displayValue;
+});
+
+del.addEventListener("click", () => {
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = displayValue;
+});
+
+two.addEventListener("click", () => {
+    displayValue += 2;
+    display.textContent = displayValue;
+})
+three.addEventListener("click", () => {
+    displayValue += 3;
+    display.textContent = displayValue;
+});
+four.addEventListener("click", () => {
+    displayValue += 4;
+    display.textContent = displayValue;
+});
+five.addEventListener("click", () => {
+    displayValue += 5;
+    display.textContent = displayValue;
+});
+six.addEventListener("click", () => {
+    displayValue += 6;
+    display.textContent = displayValue;
+});
+seven.addEventListener("click", () => {
+    displayValue += 7;
+    display.textContent = displayValue;
+});
+eight.addEventListener("click", () => {
+    displayValue += 8;
+    display.textContent = displayValue;
+});
+nine.addEventListener("click", () => {
+    displayValue += 9;
+    display.textContent = displayValue;
+});
+zero.addEventListener("click", () => {
+    displayValue += 0;
+    display.textContent = displayValue;
+});
